22d8f5b9c1a117a101839e2387c6813d1cc5cf8e
luisffelip <lfbarbosaaz@gmail.com>
2025-03-15 15:51:33 -0300

    Adicionando js ao projeto (Incluindo o modal)

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-// Seleciona o ícone do menu e o // Seleciona o ícone do menu e o modal
-const menuIcon = document.getElementById('menu-icon');
+const menuIcon = document.getElementById('menu');
 const menuModal = document.getElementById('menu-modal');
 
-// Adiciona um evento de clique ao ícone do menu
-menuIcon.addEventListener('click', () => {
-    // Alterna a classe .active no modal
+menuIcon.addEventListener('click', (event) => {
+    event.stopPropagation(); // Impede que o clique se propague para o documento
     menuModal.classList.toggle('active');
 });
 
-// Seleciona todos os links dentro do modal
+// Fecha o modal ao clicar fora dele
+document.addEventListener('click', (event) => {
+    if (!menuModal.contains(event.target) && !menuIcon.contains(event.target)) {
+        menuModal.classList.remove('active');
+    }
+});
+
 const modalLinks = document.querySelectorAll('.modal-content a');
 
-// Adiciona um evento de clique a cada link do modal
 modalLinks.forEach(link => {
     link.addEventListener('click', () => {
-        // Fecha o modal ao clicar em um link
         menuModal.classList.remove('active');
     });
 });
